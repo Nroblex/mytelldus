493c840655ad73323a82ce7252d20a30d85e6119
Anders <anders@selborn>
2015-10-19 18:36:06 +0200

    Scheduler init

--- src/main/java/Program.java
+++ src/main/java/Program.java
@@ -1,6 +1,9 @@
+import communication.Protocol;
+import communication.TelldusInterface;
 import org.apache.log4j.*;
 import org.apache.log4j.xml.DOMConfigurator;
 
+import java.io.IOException;
 
 
 /**
@@ -10,7 +13,7 @@ public class Program {
 
     static Logger iLog = LogManager.getLogger(Program.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         System.out.println("Starting");
 
@@ -21,7 +24,53 @@ public class Program {
         iLog.info("Starting...");
 
 
+        //initializeTest();
+
+        startADevice(2);
+
+
+
+
     }
+
+    private static void startADevice(int i) throws IOException{
+
+        TelldusInterface iface = new TelldusInterface("10.0.1.48", 9998, 9999);
+        String name = iface.tdGetName(i);
+
+        iface.tdTurnOn(i);
+
+        System.out.println(name);
+
+
+    }
+
+    private static void initializeTest() {
+        try {
+
+            TelldusInterface iface = new TelldusInterface("10.0.1.48", 9998, 9999);
+
+            int devices = iface.tdGetNumberOfDevices();
+
+            System.out.println(String.format("There are %s devices configurerd", devices));
+
+            for(int i = 0; i < devices; i++) {
+                int id = iface.tdGetDeviceId(i);
+                String name  = iface.tdGetName(id);
+                Protocol.DeviceType t = iface.tdGetDeviceType(id);
+                System.out.println(id+" : "+ name +" of type "+ t);
+
+            }
+
+            System.out.println("Start listening for events...");
+
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
 
     private static void setupLog4JLogging(String logFile) {
 
@@ -30,6 +79,8 @@ public class Program {
                 ConsoleAppender.SYSTEM_ERR));
 
         LogManager.getRootLogger().setLevel(Level.INFO);
+        LogManager.getRootLogger().setLevel(Level.TRACE);
+        LogManager.getRootLogger().setLevel(Level.DEBUG);
 
         DOMConfigurator.configure(logFile);
 
---
+++ src/main/java/scheduler/Scheduler.java
@@ -0,0 +1,10 @@
+package scheduler;
+
+/**
+ * Created by anders on 10/19/15.
+ */
+public class Scheduler {
+
+    public Scheduler() {}
+
+}
